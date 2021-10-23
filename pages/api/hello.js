1b1b14d669c597e4752e2fb2012d484b1cc92837
J-eld <70251176+J-eld@users.noreply.github.com>
2021-10-23 21:41:18 +1300

    added test api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+        var IPs = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress;
+
+        if (IPs.indexOf(":") !== -1) {
+            IPs = IPs.split(":")[IPs.split(":").length - 1]
+        }
+
+        return res.json({ IP: IPs.split(",")[0] });
+    } catch (err) {
+        return res.json({ message: 'got error' });
+    }
 }
