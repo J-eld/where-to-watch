31db2fff41ec924677c3d7ca28851db8bd52171e
Jad Eldik <jad.eldik@mob.co.uk>
2025-02-04 17:50:36 +0000

    added posthog support

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -3,14 +3,24 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "./get-query-client";
 import type * as React from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
+  if (typeof window !== "undefined") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    });
+  }
+
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
